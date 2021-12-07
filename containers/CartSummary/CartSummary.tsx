47ab12cd89a625ca19cb647230d3f748b243922d
Gabriel <gmr105895@gmail.com>
2021-12-07 10:20:38 +0100

    refactor(CardSummary) use `computeTotalPrice`

--- containers/CartSummary/CartSummary.tsx
+++ containers/CartSummary/CartSummary.tsx
@@ -1,28 +1,19 @@
 import styled from "styled-components";
 import CartSummaryLine from "../../components/CartSummaryLine/CartSummaryLine";
 import { LineItem } from "../../utils/types/wooCommerceTypes";
+import { computeTotalPrice } from "../../utils/prices";
 
 interface Props {
   lineItems: LineItem[];
 }
 
 const CartSummary = (props: Props) => {
-  function calculateCartTotal(lineItems: LineItem[]) {
-    if (!lineItems.length) return "0.00€";
-
-    const totalsArray = lineItems.map((lineItem) => {
-      return parseFloat(lineItem.price!) * 100 * lineItem.quantity;
-    });
-    const total = totalsArray.reduce((prev, next) => prev + next);
-    const formattedTotal = `${(total / 100).toFixed(2)}€`;
-    return formattedTotal;
-  }
 
   return (
     <Wrapper>
       <CartSummaryLine
         title="Total"
-        price={calculateCartTotal(props.lineItems)}
+        price={computeTotalPrice(props.lineItems)}
       />
     </Wrapper>
   );
