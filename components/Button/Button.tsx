88e2cdf85b4ef16c879c976a92ac10aa9ae2384f
Mczender <63108283+Mczender@users.noreply.github.com>
2021-12-07 11:53:55 +0100

    Revert "Add amazon button"

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -23,23 +23,12 @@ const Button = (props: Props) => {
 export default Button;
 
 const StyledButton = styled.button<Props>`
-  border: 1px solid ${(props) => props.theme.colors.borderYellow};
-  border-radius: 99999px;
-  background-color: ${(props) => props.theme.colors.yellow};
-  color: ${(props) => props.theme.colors.primary};
+  border: none;
+  background-color: ${(props) =>
+    props.isClicked
+      ? props.theme.colors.secondary
+      : props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.lightText};
   transition: background-color 0.2s ease;
   padding: 10px 20px;
-  &:hover{
-    border: 1px solid ${(props) => props.theme.colors.hoverBorderYellow};
-    background-color: ${(props) =>
-      props.isClicked
-        ? props.theme.colors.lightText
-        : props.theme.colors.hoverYellow};
-  }
-  &:focus{
-    outline: none;
-    border: 1px solid ${(props) => props.theme.colors.borderYellow};
-    background-color: ${(props) => props.theme.colors.lightText};
-    color: ${(props) => props.theme.colors.primary};
-  }
 `;
