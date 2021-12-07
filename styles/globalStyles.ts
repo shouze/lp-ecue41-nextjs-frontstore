214fc5df7738a3882cd9ed28e75af09f7af79d38
tom-guastapaglia <tomguasta@yahoo.fr>
2021-12-07 11:30:43 +0100

    Bouton OK

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -18,11 +18,29 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     font-family: 'Raleway'
   }
+  body{
+    margin:0;
+  }
 
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Raleway'
   }
 
+  .iJHKsF{
+    margin:0px;
+    display:flex;
+    height:60px;
+  }
+  .iJHKsF img{
+    width:150px;
+    display:block;
+    object-fit: contain;
+  }
+
+  .eXwzUs{
+    background-color:#232f3e;
+  }
+  
 `;
 
 export default GlobalStyle;
