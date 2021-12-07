c2e864af1c6035e7f5b858d1d45ec4c69ed18f27
Gabriel <gmr105895@gmail.com>
2021-12-07 11:30:14 +0100

    feat(Header) add logo

--- features/Header/Header.tsx
+++ features/Header/Header.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Heading } from "../../components";
 import Navbar from "../../containers/Navbar/Navbar";
+import Image from "next/image";  
 
 const Header = () => {
   return (
     <Wrapper>
-      <Heading textAlign="center">Aixmazone</Heading>
+      <img src="/Logo.svg" alt="Aixmazone" />
       <Navbar />
     </Wrapper>
   );
@@ -15,4 +16,9 @@ export default Header;
 
 const Wrapper = styled.div`
   width: 100%;
+  img {
+    display: block;
+    margin: 0 auto;
+    width: clamp(200px, 350px, 600px);
+  }
 `;
